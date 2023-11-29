e52e1afecbc41480e31bf306cc7b74e3369f6e67
Ivan <ivanabasin2@gmail.com>
2023-11-29 19:46:31 +0300

    Ivan_Jango Новая таблица Like_Books

--- coolsite/women/models.py
+++ coolsite/women/models.py
@@ -1,3 +1,4 @@
+import django.utils.timezone
 from django.db import models
 
 
@@ -15,7 +16,23 @@ class Pupil(models.Model):
     year_old = models.IntegerField(default=10)
     class_p = models.CharField(max_length=3)
     gender = models.CharField(max_length=1, default='м')
+    bithday = models.DateTimeField(default= django.utils.timezone.now())
+    coming_of_age = models.BooleanField(default = True)
+
+class Like_Books(models.Model):
+    name_Book = models.CharField(default= 'Book',max_length=30)
+    summary = models.CharField(default='Жили были и стали жить поживать да добра наживать',max_length=200)
+    Author = models.CharField(max_length=20)
+    date_born_author = models.DateTimeField(max_length=1)
+    date_create = models.DateTimeField(max_length=1)
+    interesting = models.BooleanField(default = True)
+    Genre = models.CharField(max_length=12)
+    size = models.FloatField(max_length=7)
+
+
 
 # Create your models here.
 
+#python manage.py makemigrations
+#python manage.py migrate
 #python manage.py sqlmigrate women 0003
